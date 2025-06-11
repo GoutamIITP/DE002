90ff6d96b22a9887f56ba334ca13de4245d23a52
Goutam Kumar Sah <goutamsah126@gmail.com>
2025-06-11 23:56:50 +0530

    Capitalize

---
+++ capitalize_sol.py
@@ -0,0 +1,34 @@
+# capitalize: Task to make the first letter of every words as capital letter
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    words = s.split(' ')
+    capitalized_words = []
+    
+    for word in words:
+        if word:
+            capitalized_word = word[0].upper() + word[1:]
+        else:
+            capitalized_word = ''
+        capitalized_words.append(capitalized_word)
+
+    return ' '.join(capitalized_words)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
